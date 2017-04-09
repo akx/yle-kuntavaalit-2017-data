028bf2a88caf59613353eda8e8dfb1db0c044561
Aarni Koskela <akx@iki.fi>
2017-04-09 23:36:59 +0300

    Auto-download muni data too

--- README.md
+++ README.md
@@ -1,16 +1,1 @@
 # yle kuntavaalit 2017 data
-
-# muni.json
-
-Generated from https://vaalit.yle.fi/tulospalvelu/kv2017/kunnat with
-
-```
-JSON.stringify(
-    Array.from(
-        document.querySelectorAll('a.yed__municipality__index__item__link'))
-        .map(a => ({name: a.innerText, link: a.href}))
-        .map(l => ({name: l.name, data: /vaalipiiri\/(.+?)\/kunta\/(\d+)/.exec(l.link)}))
-        .map(l => ({name: l.name, electorate: l.data[1], municipality: l.data[2]})
-    )
-)
-```
--- download.py
+++ download.py
@@ -7,14 +7,18 @@ sess = requests.Session()
 sess.headers['user-agent'] = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3063.4 Safari/537.36'
 sess.headers['referer'] = 'https://vaalit.yle.fi/'
 
-muni_data = json.load(open('muni.json', encoding='utf-8'))
+with open('municipalities.json', 'wb') as outf:
+    resp = sess.get('https://vaalit.yle.fi/content/kv2017/municipalities.json')
+    outf.write(resp.content)
+    muni_data = resp.json()['municipalities']
+
 latest_version_data = sess.get('https://vaalit.yle.fi/content/kv2017/latestVersion.json').json()
 url_template = 'https://vaalit.yle.fi/content/kv2017/{version}/electorates/{electorate}/municipalities/{municipality}/partyAndCandidateResults.json'
 
 for version in range(latest_version_data['mainVersion'], 1, -1):
     for muni in muni_data:
-        url = url_template.format(version=version, electorate=muni['electorate'], municipality=muni['municipality'])
-        output_file = 'data/{version}/{name}.json'.format(version=version, name=muni['name'])
+        url = url_template.format(version=version, electorate=muni['edid'], municipality=muni['muid'])
+        output_file = 'data/{version}/{name}.json'.format(version=version, name=muni['name']['fi'])
         if os.path.exists(output_file):
             continue
         output_dir = os.path.dirname(output_file)
