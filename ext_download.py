0088eddf5390ea6d9ead4493c6f4b76fd686dd91
Aarni Koskela <akx@iki.fi>
2017-04-10 00:48:33 +0300

    Fix a bug or two in ext_data downloader

--- ext_download.py
+++ ext_download.py
@@ -10,13 +10,12 @@ ext_url_template = 'https://vaalit.yle.fi/content/kv2017/{version}/electorates/{
 def download_ext_data(version):
     sess = get_session()
     for muni_fn in glob.glob('data/{version}/*.json'.format(version=version)):
-        with open(muni_fn) as infp:
+        with open(muni_fn, encoding='utf-8') as infp:
             muni = json.load(infp)
         name = muni['calculationStatus']['name']['fi']
         perc = float(muni['calculationStatus']['calculationStatusPercent'])
         if perc < 100:
-            print('%s: %.2f%% less than 100%% percent, skipping' % (name, perc))
-            continue
+            print('%s: %.2f%% less than 100%% percent!' % (name, perc))
         for district in muni['pollingDistricts']:
             url = ext_url_template.format(
                 version=version,
